3dc1ae3f70d6402acbc241c70aefa81b8d861fcc
Emily <emelizadaniels@gmail.com>
2022-11-22 15:13:26 -0500

    commit tree 3cea93bb2d4f0b536e71af4a41d1120225b8a75b

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,21 @@
-## code your solution here. 
+require 'pry'
+
+class Cat
+attr_accessor :name
+
+
+
+  def cat_name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+  def meow
+    puts "meow!"
+  end
+end
+c1 = Cat.new
+c1.name= "Gourdie"
+binding.pry
